3b993b9429bc7f7816d77df523127fcbe7cd6905
CihanAkkus <cihann.akkus2@gmail.com>
2024-06-11 19:15:07 +0300

    commit

---
+++ FibonacciWithRecursive.py
@@ -0,0 +1,17 @@
+def fibonacci(a,b,n,c = 0):
+    sum = a + b
+    print(sum)
+    if (c >= n):
+        return
+    if (c % 2 == 0):
+        fibonacci(a,sum,n,c+1)
+    else:
+        fibonacci(sum,b,n,c+1)
+
+
+a,b = 1,1
+
+print(a)
+print(b)
+
+fibonacci(a,b,10)
