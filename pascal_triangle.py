4d023db238a2fcbbf30497c9f17ad7fbf8ff069d
CihanAkkus <cihann.akkus2@gmail.com>
2024-06-29 16:46:45 +0300

    pascal triangle

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -10,12 +10,9 @@ for i in range(1,row):
     row_list.insert(0, 1)
     row_list.insert(-1, 1)
 
-
     for j in range(1,i):
         sum = (row_list_copy[j-1] + row_list_copy[j])
         row_list.insert(j , sum)
-
-
         sum = 0
 
     row_list_copy = row_list.copy()
