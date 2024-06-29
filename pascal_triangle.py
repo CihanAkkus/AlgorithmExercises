5b0851effab7230bb9f016bb252f568b126cb101
CihanAkkus <cihann.akkus2@gmail.com>
2024-06-29 16:43:49 +0300

    the completed version

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,24 +1,25 @@
 row = int(input("Please enter a row = "))
 
-list = [[1],[1,1]]
+list = [[1]]
 row_list = []
 
-i = 0
-j = 0
-k = 0
-sum = 3
+sum = 0
 
-while (i < row):
+for i in range(1,row):
     row_list.clear()
     row_list.insert(0, 1)
     row_list.insert(-1, 1)
 
-    a = 1
-    while (j < row):
-        row_list.insert(i , sum)
-        a += 1
-        j += 1
 
-    i += 1
+    for j in range(1,i):
+        sum = (row_list_copy[j-1] + row_list_copy[j])
+        row_list.insert(j , sum)
+
+
+        sum = 0
+
+    row_list_copy = row_list.copy()
+    list.append(row_list.copy())
+
 
 print(list)
