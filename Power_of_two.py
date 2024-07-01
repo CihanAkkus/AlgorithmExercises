107f3ea6ab90fae7c8ad6c19884070b6f8cf5031
CihanAkkus <cihann.akkus2@gmail.com>
2024-07-01 23:08:09 +0300

    power of two without loops

--- Power_of_two.py
+++ Power_of_two.py
@@ -1,1 +1,9 @@
-def control(num):
+def is_power_of_two(num):
+    num = abs(num)
+    if num == 0:
+        return False
+    return (num & (num - 1)) == 0
+
+num = int(input("Please enter a number = "))
+
+print(is_power_of_two(num))
