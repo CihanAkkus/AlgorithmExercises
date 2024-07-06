dd9b920824b769fd54d8c65a52d5b9af7179c4d5
CihanAkkus <cihann.akkus2@gmail.com>
2024-07-06 21:17:42 +0300

    number of 1 bits

---
+++ number_of_1_bits.py
@@ -0,0 +1,19 @@
+num = int(input("Please enter a number = "))
+
+i = 0
+list = []
+
+while (True):
+    remainder = num % 2
+    list.append(remainder)
+    num = num // 2
+    if (num == 0):
+        list.reverse()
+        break
+
+sum = 0
+
+for j in list:
+    sum += j
+
+print(sum)
