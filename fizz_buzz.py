8c55182679fb8a50934419cfd51b8ed51ed36dc4
CihanAkkus <cihann.akkus2@gmail.com>
2024-07-10 00:35:23 +0300

    Fizz Buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,15 @@
+list = []
+
 def fizz_buzz(num):
+    for i in range(1,num+1):
+        if (i % 15 == 0):
+            list.append("FizzBuzz")
+        elif (i % 3 == 0):
+            list.append("Fizz")
+        elif (i % 5 == 0):
+            list.append("Buzz")
+        else:
+            list.append(str(i))
+    return list
+
+print(fizz_buzz(15))
