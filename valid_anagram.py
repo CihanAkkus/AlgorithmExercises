4bf8d3bf9c5d62f1d6f2a3f50186cdfeb7ad7591
CihanAkkus <cihann.akkus2@gmail.com>
2024-07-15 21:06:13 +0300

    Valid Anagram

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,4 +1,13 @@
 def isAnagram(s,t):
-    for i in s:
-        for j in t:
-            if (i == j):
+    if (len(s) == len(t)):
+        for i in s:
+            for j in t:
+                if (i == j):
+                    character_to_remove = j
+                    t = t.replace(character_to_remove, "")
+        if (t == ""):
+            return True
+        else:
+            return False
+    else:
+        return False
