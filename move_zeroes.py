662f689137ef1a071e8c93710d9bc48d1413ed9e
CihanAkkus <cihann.akkus2@gmail.com>
2024-07-19 11:28:01 +0300

    Move Zeroes

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,7 +1,10 @@
 def moveZeroes(nums):
-    for i in range(0,len(nums)-1):
-        if (nums[i] == 0):
-            nums[i],nums[i+1] = nums[i+1],nums[i]
+    a = 0
+    for k in nums:
+        if (k == 0):
+            a += 1
+    for j in range(0,a):
+        for i in range(0, len(nums) - 1):
+            if (nums[i] == 0):
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
     return nums
-
-print(moveZeroes([1,0,3,0,12]))
