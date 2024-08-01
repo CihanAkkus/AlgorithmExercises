6a97c86c134b3fce3c93e84eb88d4c0a48cc17f4
CihanAkkus <cihann.akkus2@gmail.com>
2024-08-01 16:41:51 +0300

    Happy Number

--- happy_number.py
+++ happy_number.py
@@ -5,7 +5,6 @@ def isHappy(n):
         for i in range(0, len(str(n))):
             sum += int(copy_num[i]) ** 2
         n = sum
-        print(n)
         if (sum == 1):
             return True
         sum = 0
