46d7f44bb7b26834cfc172fbde12babadf6665f5
CihanAkkus <cihann.akkus2@gmail.com>
2024-08-07 21:07:59 +0300

    Counting Bits

--- counting_bits.py
+++ counting_bits.py
@@ -1,2 +1,14 @@
 def countBits(n):
-    for i in range(0,n)
+    list = []
+    for i in range(0,n+1):
+        count = 0
+        while (True):
+            remainder = i % 2
+            i = i // 2
+            if (remainder == 1):
+                count += 1
+            if (i == 0):
+                break
+        list.append(count)
+    return list
+print(countBits(5))
