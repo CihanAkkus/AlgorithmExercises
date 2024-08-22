73394a61827163e213fd49f4d8cf2a15726f0436
CihanAkkus <cihann.akkus2@gmail.com>
2024-08-22 23:43:28 +0300

    Intersection of two arrays

--- intersection_of_two_Arrays.py
+++ intersection_of_two_Arrays.py
@@ -1,10 +1,13 @@
 def intersection(nums1,nums2):
+
     list = []
     copy_nums2 = nums2[:]
+
     for i in nums1:
         if (i in copy_nums2):
             list.append(i)
             for j in nums2:
                 if (j == i):
                     copy_nums2.remove(j)
+                    
     return list
