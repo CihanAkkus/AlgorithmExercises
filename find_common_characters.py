ed4312c7ce4048510c64464970370ca07db67f6f
CihanAkkus <cihann.akkus2@gmail.com>
2024-09-17 23:39:42 +0300

    new commit

--- find_common_characters.py
+++ find_common_characters.py
@@ -1,5 +1,18 @@
-def commonChars():
+def commonChars(words):
+    list = []
+    for i in range(0,len(words)-2):
 
-    print("first line")
+        for j in range(0,len(words[i])-1):
 
-commonChars()
+            if (words[i][j] in list):
+                continue
+
+
+            for k in range(0,len(words[i + 1])-1):
+
+                if (words[i][j] == words[i][k]):
+                    list.append(words[i][j])
+
+    return list
+
+print(commonChars(["bella","label","roller"]))
